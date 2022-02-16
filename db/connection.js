258f93d6a2589e381fbd931207d754ff1903d708
hjohnsick <hjohnsick@yahoo.com>
2022-02-15 20:35:53 -0500

    edited connection

--- db/connection.js
+++ db/connection.js
@@ -4,8 +4,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'LoserKids051!',
+        user: '',
+        password: '',
         database: 'election'
     },
     console.log('Connected to the election database.')
